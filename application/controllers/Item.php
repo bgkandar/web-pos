da40b02ff91b0b58d6fe1f97620efd27c9458c38
Bangkandar <misimatupang@gmail.com>
2019-05-26 23:48:00 +0700

    commit update menu item

---
+++ application/controllers/Item.php
@@ -0,0 +1,92 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Item extends CI_Controller {
+	public function __construct() {
+		parent::__construct();
+		check_not_login();
+		// check_admin();
+		$this->load->model(['item_m', 'category_m', 'unit_m']);
+	}
+	
+	public function index()
+	{
+		$data['row'] = $this->item_m->get();
+		$this->template->load('template', 'product/item/item_data', $data);
+	}
+
+	public function add()
+	{
+		$item = new stdClass();
+        $item->item_id = null;
+        $item->barcode = null;
+		$item->name = null;
+        $item->harga = null;
+        
+        $query_category = $this->category_m->get();
+
+        $query_unit = $this->unit_m->get();
+        $unit[null] = '- Pilih -';
+        foreach($query_unit->result() as $unt) {
+            $unit[$unt->unit_id] = $unt->name;
+        }
+
+		$data = array(
+			'page' => 'add',
+            'row' => $item,
+            'category' => $query_category,
+            'unit' => $unit, 
+            'selectedunit' => null
+		);
+		$this->template->load('template', 'product/item/item_form', $data);
+	}
+
+	public function edit($id) {
+		$query = $this->item_m->get($id);
+		if ($query->num_rows() > 0 ) {
+			$item = $query->row();
+			$query_category = $this->category_m->get();
+
+            $query_unit = $this->unit_m->get();
+            $unit[null] = '- Pilih -';
+            foreach($query_unit->result() as $unt) {
+                $unit[$unt->unit_id] = $unt->name;
+            }
+
+            $data = array(
+                'page' => 'edit',
+                'row' => $item,
+                'category' => $query_category,
+                'unit' => $unit, 
+                'selectedunit' => $item->unit_id
+            );
+            $this->template->load('template', 'product/item/item_form', $data);
+		} else {
+			echo "<script>alert('Data tidak ditemukan!');";
+			echo "window.location='".site_url('user')."'</script>";
+		}
+	}
+
+	public function process() {
+		$post = $this->input->post(null, TRUE);
+		if (isset($_POST['add'])) {
+			$this->item_m->add($post);
+		} else if (isset($_POST['edit'])) {
+			$this->item_m->edit($post);
+		}
+
+		if ($this->db->affected_rows() > 0) {
+			$this->session->set_flashdata('success', 'Data berhasil disimpan');
+		}
+			redirect('item');
+	}
+
+	public function delete($id) {
+		$this->item_m->del($id);
+
+		if ($this->db->affected_rows() > 0) {
+			$this->session->set_flashdata('success', 'Data berhasil dihapus');
+		}
+            redirect('item');
+	}
+}
---
+++ application/views/product/item/item_form.php
@@ -0,0 +1,62 @@
+<section class="content-header">
+	<h1>
+		Items
+		<small>Data Barang</small>
+	</h1>
+	<ol class="breadcrumb">
+		<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+		<li class="active">Items</li>
+	</ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+	<div class="box">
+		<div class="box-header">
+			<h3 class="box-title"><?= ucfirst($page); ?> items</h3>
+			<div class="pull-right">
+				<a href="<?= site_url('item')?>" class="btn btn-warning btn-flat"><i class="fa fa-undo"></i> Back</a>
+			</div>
+
+		</div> 
+		<div class="box-body">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <form action="<?= site_url('item/process')?>" method="post">
+                        <div class="form-group">
+                            <label for="">Barcode *</label>
+                            <input type="hidden" name="id" value="<?= $row->item_id; ?>">
+                            <input type="text" name="barcode" value="<?= $row->barcode; ?>" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Product Name *</label>
+                            <input type="text" name="product_name" value="<?= $row->name; ?>" class="form-control" required>
+                        </div> 
+                        <div class="form-group">
+                            <label for="">Category *</label>
+                            <select name="category" class="form-control">
+                                <option value="">- Pilih -</option>
+                                <?php foreach($category->result() as $key => $data) { ?>
+                                    <option value="<?= $data->category_id?>" <?= $data->category_id == $row->category_id ? "selected" : null?>><?= $data->name?></option>
+                                <?php } ?>
+                            </select>
+                        </div> 
+                        <div class="form-group">
+                            <label for="">Unit *</label>
+                                <?php echo form_dropdown('unit', $unit, $selectedunit,
+                                ['class' => 'form-control', 'required' => 'required'])?>
+                        </div> 
+                        <div class="form-group">
+                            <label for="">Harga *</label>
+                            <input type="number" name="harga" value="<?= $row->harga; ?>" class="form-control" required>
+                        </div> 
+                        <div class="form-group">
+                            <button class="btn btn-primary btn-flat" name="<?= $page; ?>" type="submit"><i class="fa fa-paper-plane"></i> Save</button>    
+                            <button class="btn btn-secondary btn-flat" type="reset">Reset</button>    
+                        </div>
+                    </form>
+                </div>
+            </div>
+		</div>
+	</div>
+</section>
